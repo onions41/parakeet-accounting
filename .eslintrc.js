19b7922e83de59f7e0366beb24b454c833db5999
onions41 <kblee89@outlook.com>
2022-12-31 20:07:31 -0800

    Defined rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,19 +3,27 @@ module.exports = {
     browser: true,
     es2021: true
   },
-  extends: [
-    'plugin:react/recommended',
-    'standard'
-  ],
-  overrides: [
-  ],
+  extends: ["plugin:react/recommended", "standard"],
+  overrides: [],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+    ecmaVersion: "latest",
+    sourceType: "module"
   },
-  plugins: [
-    'react'
-  ],
+  plugins: ["react"],
   rules: {
+    "space-before-function-paren": [
+      "error",
+      {
+        anonymous: "never",
+        named: "never",
+        asyncArrow: "always"
+      }
+    ],
+    quotes: ["error", "double"],
+    "arrow-parens": ["error", "always"],
+    "no-unused-vars": 1,
+    "react/prop-types": 0,
+    "no-console": 1,
+    "multiline-ternary": 0
   }
 }
