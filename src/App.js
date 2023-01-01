18ec23ac4d87f0f1aecc1d5610b4d34b62094119
onions41 <kblee89@outlook.com>
2022-12-31 21:47:24 -0800

    Starting layouts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 // Module imports
+import { Container } from "@mui/system"
+import { Box } from "@mui/material"
 import React from "react"
 
 // Internal imports
 
 export default function App() {
   return (
-    <div>Coming Soon</div>
+    <Container>
+      <Box>This is Boyan Dong CPA sdfkjdfsd dfjsdkfsjdfs sdflkjdfsd sdfsdfsd</Box>
+    </Container>
   )
 }
