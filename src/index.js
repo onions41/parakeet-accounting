8ccfec1f1accd32c113b260393307facead19ecc
onions41 <kblee89@outlook.com>
2023-01-05 17:48:00 -0800

    Provided theme to App

--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import CssBaseline from "@mui/material/CssBaseline"
+import ThemeProvider from "@mui/material/styles/ThemeProvider"
 
 // Internal imports
 import App from "./App"
+import theme from "./theme/theme"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
-    <CssBaseline />
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 )
