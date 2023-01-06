1467eaeb661e68f27058be7f34f0740bbb3a0820
onions41 <kblee89@outlook.com>
2023-01-05 17:55:28 -0800

    Temporary layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 // Module imports
-import { Container } from "@mui/system"
-import { Box } from "@mui/material"
-import React from "react"
+import Container from "@mui/system/Container"
+import Box from "@mui/material/Box"
+import styled from "@mui/material/styles/styled"
 
 // Internal imports
+import Menu from "./menu/Menu"
+
+// Prevents anchor links from colliding with the sticky DesktopMenu (> lg)
+const SectionBox = styled(Box)(({ theme }) => ({
+  [theme.breakpoints.up("lg")]: {
+    "&:target": {
+      marginTop: "-91px",
+      paddingTop: "91px" // height of the menu container
+    }
+  }
+}))
 
 export default function App() {
   return (
-    <Container>
-      <Box>This is Boyan Dong CPA sdfkjdfsd dfjsdkfsjdfs sdflkjdfsd sdfsdfsd</Box>
+    <Container
+      maxWidth="xl"
+      sx={{
+        px: 3,
+        border: "1px black solid" // Guideline, remove later
+      }}
+    >
+      <Menu />
+      <SectionBox id="qbo-advisor">Lets see what happes now</SectionBox>
     </Container>
   )
 }
