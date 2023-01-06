e5d6ac3c1aed639d220cec2affc3eb607c43ba03
onions41 <kblee89@outlook.com>
2023-01-05 17:58:06 -0800

    Defined links for the menu bar

---
+++ src/menu/links.js
@@ -0,0 +1,8 @@
+const links = [
+  { name: "Home", href: "#home" },
+  { name: "Services", href: "#services" },
+  { name: "QBO Advisor", href: "#qbo-advisor" },
+  { name: "Contact Us", href: "#contact-us" }
+]
+
+export default links
