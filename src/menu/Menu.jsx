dba0c7d230de67b24f2766b7c8aa01ee9e88b25c
onions41 <kblee89@outlook.com>
2023-01-07 21:01:04 -0800

    Removed useTheme hook

--- src/menu/Menu.jsx
+++ src/menu/Menu.jsx
@@ -1,17 +1,15 @@
 // Module imports
 import useMediaQuery from "@mui/material/useMediaQuery"
-import useTheme from "@mui/material/styles/useTheme"
 
 // Internal imports
 import DesktopMenu from "./DesktopMenu"
 import MobileMenu from "./MobileMenu"
 
 export default function Menu() {
-  const theme = useTheme()
   // Either mobile or desktop
   // Screen width >= lg is desktop, otherwise mobile
   // lg breakpoint is set at 1024 (check theme file)
-  const isMobile = useMediaQuery(theme.breakpoints.down("lg"))
+  const isMobile = useMediaQuery((theme) => theme.breakpoints.down("lg"))
 
   return isMobile ? <MobileMenu /> : <DesktopMenu />
 }
