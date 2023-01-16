1a20661dc4ccd829e3a25a5f3d3abfd6a964530c
onions41 <kblee89@outlook.com>
2023-01-16 11:17:39 -0800

    Tweaked colors

--- src/theme/palette.js
+++ src/theme/palette.js
@@ -3,10 +3,20 @@ const palette = {
     main: "#00C3FF", // parakeet blue
     contrastText: "#FFFFFF"
   },
+  secondary: {
+    main: "#5bd53c",
+    light: "#63e942"
+  },
   text: {
     primary: "#292b30", // parakeet black
     secondary: "#969b91" // lime gray
+  },
+  footer: {
+    main: "#393a3c",
+    contrastText: "#FFFFFF"
   }
 }
 
 export default palette
+
+// #393a3c
