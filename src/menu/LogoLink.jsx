2f6f5050f438cd9d6a437773e24e4ea89bb69826
onions41 <kblee89@outlook.com>
2023-01-16 11:20:10 -0800

    Made the logo stack properly when small screen

--- src/menu/LogoLink.jsx
+++ src/menu/LogoLink.jsx
@@ -1,38 +1,46 @@
 import Stack from "@mui/material/Stack"
 import Typography from "@mui/material/Typography"
 import Link from "@mui/material/Link"
+import useTheme from "@mui/material/styles/useTheme"
+import useMediaQuery from "@mui/material/useMediaQuery"
+import Box from "@mui/material/Box"
 
 export default function LogoLink({ palette }) {
+  const { breakpoints } = useTheme()
+  const menuBp = useMediaQuery(breakpoints.up("menu"), { noSsr: true })
+
   return (
-    <Link
-      href="/"
-      underline="none"
-    >
-      <Stack
-        direction="row"
-        alignItems="center"
-        spacing={2}
+    <Box sx={{ width: menuBp ? "auto" : "200px" }}>
+      <Link
+        href="/"
+        underline="none"
       >
-        {/* Logo */}
-        <img
-          width="40px"
-          height="40px"
-          style={{ position: "relative", bottom: 4 }}
-          src={`${process.env.PUBLIC_URL}/favicon.svg`}
-        />
-        {/* Company Name */}
-        <Typography
-          component="h1"
-          sx={{
-            fontFamily: "'Axios Pro', sans-serif",
-            fontSize: 25,
-            fontWeight: 600,
-            color: palette.text.primary
-          }}
+        <Stack
+          direction="row"
+          alignItems="center"
+          spacing={2}
         >
-          Parakeet Accounting
-        </Typography>
-      </Stack>
-    </Link>
+          {/* Logo */}
+          <img
+            width="40px"
+            height="40px"
+            style={{ position: "relative", bottom: 4 }}
+            src={`${process.env.PUBLIC_URL}/favicon.svg`}
+          />
+          {/* Company Name */}
+          <Typography
+            component="h1"
+            sx={{
+              fontFamily: "'Axios Pro', sans-serif",
+              fontSize: 25,
+              fontWeight: 600,
+              color: palette.text.primary
+            }}
+          >
+            Parakeet Accounting
+          </Typography>
+        </Stack>
+      </Link>
+    </Box>
   )
 }
