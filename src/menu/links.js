c6c90d873573289d5961744c6d2ade1729705aae
onions41 <kblee89@outlook.com>
2023-01-16 11:16:46 -0800

    Removed #s

--- src/menu/links.js
+++ src/menu/links.js
@@ -1,8 +1,8 @@
 const links = [
-  { name: "Home", href: "#home" },
-  { name: "Services", href: "#services" },
-  { name: "QBO Advisor", href: "#qbo-advisor" },
-  { name: "Contact Us", href: "#contact-us" }
+  { name: "Home", href: "home" },
+  { name: "Services", href: "services" },
+  { name: "QBO Advisor", href: "qbo-advisor" },
+  { name: "Contact Us", href: "contact-us" }
 ]
 
 export default links
